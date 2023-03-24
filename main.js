f78d9e3196ebfef3a355d6bfd0861debd2e9b7d7
Egnitius <127835355+Egnitius@users.noreply.github.com>
2023-03-24 10:43:06 +0000

    Script

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ faqs.forEach(faq => {
         }
     })
 })
+
+
+//show/hide Nav
+const menuBtn = document.querySelector("#men-btn");
+const closeBtn = document.querySelector("#close-btn");
+const menu = document.querySelector(".nav_items");
+
+//////////open nav menu
+menuBtn.addEventListener("click", () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+//////////close nav menu
+closeBtn.addEventListener("click", () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
